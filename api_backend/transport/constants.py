ecea61fc90b46a8d545ed46eb73d584ad77fa38f
Deependra Singh <deependrasg@gmail.com>
2020-09-19 02:04:20 +0530

    Add migrations file and constants

---
+++ api_backend/transport/constants.py
@@ -0,0 +1,9 @@
+constant = {
+    "SuccessfullyUpdated": "Successfully Updated",
+    "SuccessfullyDeleted": "Successfully Deleted",
+    "Error": "error",
+    # Manufacturer Model Constants
+    "InvalidManufacturerId": "Manufacturer Objects {} does not exist",
+    # Car Model Constants
+    "InvalidCarId": "Car Objects {} does not exist",
+}
