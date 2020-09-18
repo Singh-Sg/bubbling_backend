dfd6bd3d0052679db3df9ffe2d4be2018fd0051c
Deependra Singh <deependrasg@gmail.com>
2020-09-19 02:16:04 +0530

    Add user_uuid fields in user table

--- user_manager/users/models.py
+++ user_manager/users/models.py
@@ -2,7 +2,10 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
+def uuid_str():
+    return str(uuid.uuid4())
 
 class CustomUserManager(BaseUserManager):
     """
@@ -41,7 +44,7 @@ class Users(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
     api_backend_token = models.CharField(max_length=100, null=True, blank=True)
-
+    user_uuid = models.CharField(max_length=100, default=uuid_str, unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
