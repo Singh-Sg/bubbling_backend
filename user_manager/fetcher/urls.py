f86639c42c4e63a3445751634c969fac2fb30859
Deependra Singh <deependrasg@gmail.com>
2020-09-19 03:28:24 +0530

    Integrate car apis in 2nd service

--- user_manager/fetcher/urls.py
+++ user_manager/fetcher/urls.py
@@ -15,4 +15,26 @@ urlpatterns = [
         views.ManufacturerViews.as_view(),
         name="manufacturer",
     ),
+    # This causes a car objects to be created and returns created car objects.
+    path(
+        "car/",
+        views.CarViews.as_view(),
+        name="car_create",
+    ),
+    # This returns, updates, deletes the specified car objects
+    path(
+        "car/<str:car_id>/",
+        views.CarViews.as_view(),
+        name="car",
+    ),
+    path(
+        "manufacturer-list/",
+        views.get_all_manufacturer_list,
+        name="manufacturer_list",
+    ),
+    path(
+        "car-list/",
+        views.get_all_car_list,
+        name="car_list",
+    )
 ]
--- user_manager/fetcher/views.py
+++ user_manager/fetcher/views.py
@@ -6,7 +6,7 @@ from rest_framework import status, generics
 from django.http import Http404, JsonResponse, StreamingHttpResponse
 import json
 from .constants import constant
-
+from rest_framework.decorators import api_view
 
 api_backend_base_url="http://127.0.0.1:8001"
 
@@ -25,14 +25,14 @@ class ManufacturerViews(APIView):
         :return: Manufacturer data and status 201 created
         """
         try:
-	        resp = requests.post(
-	        api_backend_base_url + "/manufacturer/",
-	        data=request.data,
-	        headers={"Authorization": "Token " + request.user.api_backend_token},
-	        )
-	        return JsonResponse(json.loads(resp.content), status=resp.status_code)
+            resp = requests.post(
+            api_backend_base_url + "/manufacturer/",
+            data=request.data,
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
         except Exception as e:
-        	return Response(
+            return Response(
                 {
                     constant["Error"]: constant["ApiBackendError"]
                 },
@@ -48,13 +48,13 @@ class ManufacturerViews(APIView):
         :return: returns specified manufacturer object
         """
         try:
-	        resp = requests.get(
-	        api_backend_base_url + "/manufacturer/"+manufacturer_id+"/",
-	        headers={"Authorization": "Token " + request.user.api_backend_token},
-	        )
-	        return JsonResponse(json.loads(resp.content), status=resp.status_code)
+            resp = requests.get(
+            api_backend_base_url + "/manufacturer/"+manufacturer_id+"/",
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
         except Exception as e:
-        	return Response(
+            return Response(
                 {
                     constant["Error"]: constant["ApiBackendError"]
                 },
@@ -70,14 +70,14 @@ class ManufacturerViews(APIView):
         :return: Updated object response with status 200 OK
         """
         try:
-	        resp = requests.patch(
-	        api_backend_base_url + "/manufacturer/"+manufacturer_id+"/",
-	        data=request.data,
-	        headers={"Authorization": "Token " + request.user.api_backend_token},
-	        )
-	        return JsonResponse(json.loads(resp.content), status=resp.status_code)
+            resp = requests.patch(
+            api_backend_base_url + "/manufacturer/"+manufacturer_id+"/",
+            data=request.data,
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
         except Exception as e:
-        	return Response(
+            return Response(
                 {
                     constant["Error"]: constant["ApiBackendError"]
                 },
@@ -94,15 +94,136 @@ class ManufacturerViews(APIView):
         :return: empty response with status 200 OK
         """
         try:
-	        resp = requests.delete(
-	        api_backend_base_url + "/manufacturer/"+manufacturer_id+"/",
-	        headers={"Authorization": "Token " + request.user.api_backend_token},
-	        )
-	        return JsonResponse(json.loads(resp.content), status=resp.status_code)
+            resp = requests.delete(
+            api_backend_base_url + "/manufacturer/"+manufacturer_id+"/",
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
         except Exception as e:
-        	return Response(
+            return Response(
                 {
                     constant["Error"]: constant["ApiBackendError"]
                 },
                 status.HTTP_404_NOT_FOUND,
             )
+
+
+class CarViews(APIView):
+    """
+    CarViews is used to create, update, fetch and delete objects
+    """
+    permission_classes = [permissions.IsAdminUser]
+
+    @classmethod
+    def post(self, request, format=None):
+        """
+        This causes a car to be created
+        :param request:
+        :return: car data and status 201 created
+        """
+        try:
+            resp = requests.post(
+            api_backend_base_url + "/car/",
+            data=request.data,
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
+        except Exception as e:
+            return Response(
+                {
+                    constant["Error"]: constant["ApiBackendError"]
+                },
+                status.HTTP_404_NOT_FOUND,
+            )
+
+
+    @classmethod
+    def get(cls, request, car_id):
+        """
+        returns specified car
+        :param request:
+        :param car_id
+        :return: returns specified manufacturer object
+        """
+        try:
+            resp = requests.get(
+            api_backend_base_url + "/car/"+car_id+"/",
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
+        except Exception as e:
+            return Response(
+                {
+                    constant["Error"]: constant["ApiBackendError"]
+                },
+                status.HTTP_404_NOT_FOUND,
+            )
+
+    @classmethod
+    def patch(cls, request, car_id):
+        """
+        This updates the specified car
+        :param request:
+        :param car_id:
+        :return: Updated object response with status 200 OK
+        """
+        try:
+            resp = requests.patch(
+            api_backend_base_url + "/car/"+car_id+"/",
+            data=request.data,
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
+        except Exception as e:
+            return Response(
+                {
+                    constant["Error"]: constant["ApiBackendError"]
+                },
+                status.HTTP_404_NOT_FOUND,
+            )
+
+
+    @classmethod
+    def delete(cls, request, car_id):
+        """
+        This deletes the specified car
+        :param car_id:
+        :return: empty response with status 200 OK
+        """
+        try:
+            resp = requests.delete(
+            api_backend_base_url + "/car/"+car_id+"/",
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+            return JsonResponse(json.loads(resp.content), status=resp.status_code)
+        except Exception as e:
+            return Response(
+                {
+                    constant["Error"]: constant["ApiBackendError"]
+                },
+                status.HTTP_404_NOT_FOUND,
+            )
+
+
+@api_view(["GET"])
+def get_all_manufacturer_list(request):
+    """
+    Get manufacturer data
+    """
+    resp = requests.get(
+            api_backend_base_url + "/manufacturer-list/",
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+    return JsonResponse({"manufacturer_data": json.loads(resp.content)}, status=resp.status_code)
+
+
+@api_view(["GET"])
+def get_all_car_list(request):
+    """
+    Get car data
+    """
+    resp = requests.get(
+            api_backend_base_url + "/car-list/",
+            headers={"Authorization": "Token " + request.user.api_backend_token},
+            )
+    return JsonResponse({"car_data":json.loads(resp.content)}, status=resp.status_code)
