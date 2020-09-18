1562873cbb1fca6fa566253dfac56e06d409390f
Deependra Singh <deependrasg@gmail.com>
2020-09-19 03:25:31 +0530

    Update service user table same like service 2

--- api_backend/api_backend/settings.py
+++ api_backend/api_backend/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'transport.Users'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
--- api_backend/transport/admin.py
+++ api_backend/transport/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Users
 from .models import (
     Manufacturer,
     Car,
 )
 
+
+
+class UsersUserAdmin(UserAdmin):
+    model = Users
+    list_display = ('email', 'is_staff', 'is_active', 'first_name', 'last_name')
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name',)}),
+
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', "user_permissions")}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'first_name', 'last_name')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    readonly_fields = ('email',)
+
+
+admin.site.register(Users, UsersUserAdmin)
 admin.site.register(Manufacturer)
 admin.site.register(Car)
+
